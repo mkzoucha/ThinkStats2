25f3adb190a11735e49eba82bd22279f381b8d35
mkzoucha <mzoucha@my365.bellevue.edu>
2021-09-09 14:44:45 -0600

    Initial Version

---
+++ ZouchaM_Completed_Assignments/ZouchaM_Week2_Assignment.py
@@ -0,0 +1,41 @@
+# DSC 530
+# Assignment: Week 2
+# Author: Michael Zoucha
+# 09 September 2021
+
+
+print("Hello World! I wonder why that is always the default coding text to start with")
+
+x = 5 + 12
+
+print(x)
+
+y = 12 - 5
+
+print(y)
+
+z = 2 * 3
+
+print(z)
+
+d = 6 / 2
+
+print(d)
+
+string = "My name is " + "Mike Zoucha"
+
+print(string)
+
+list = [1, 2, 3, 4]
+
+print(type(list))
+print(list)
+
+list.append(5)
+
+print(list)
+
+tuple = (1, 2, 3, 4)
+
+print(type(tuple))
+print(tuple)
